ea876be8842a7b9047043aeb13bcd5b656b25069
Tavish Pegram <tapegram@gmail.com>
2020-06-17 13:39:25 -0500

    working as a script

---
+++ src/branch.py
@@ -0,0 +1,24 @@
+from subprocess import Popen, PIPE, STDOUT
+
+def branch():
+    process = Popen(['git branch'], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, )
+    output, _ = process.communicate()
+    print(str(output.decode("utf-8")))
+
+def create_branch(name):
+    process = Popen(['git checkout -b {}'.format(name)], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, )
+    output, _ = process.communicate()
+    print(str(output.decode("utf-8")))
+
+
+if __name__ == '__main__':
+    create_branch("testing")
+#
+# def push(self):
+#     """git push"""
+#     if self.git_exec:
+#         process = Popen([self.git_exec, ' git push'], stdin=PIPE, stdout=PIPE, stderr=STDOUT, )
+#     else:
+#         process = Popen(['git push'], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, )
+#     output, _ = process.communicate()
+#     return str("Push completed.{}".format(str(output.decode("utf-8"))))
