f0f8a9c5179df391e2960e49efc2d34ebe6ffc86
Tavish Pegram <tapegram@gmail.com>
2020-06-18 11:43:26 -0500

    allow setting workstream

--- src/config.py
+++ src/config.py
@@ -1,2 +1,17 @@
+import os
+from subprocess import Popen, PIPE
+
+
+class GetEnvironmentVariableEffect(object):
+    def __init__(self, key):
+        self.key = key
+
+    def run(self):
+        return os.getenv(self.key)
+
+
+MOBN_WORKSTREAM_NAME = "MOBN_WORKSTREAM_NAME"
+
+
 def get_workstream_name():
-    return "matcha"
+    return GetEnvironmentVariableEffect(MOBN_WORKSTREAM_NAME)
