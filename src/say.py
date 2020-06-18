6d7fe9f24b5f317d16e973fdf426aeb94ad31313
Tavish Pegram <tapegram@gmail.com>
2020-06-18 09:18:14 -0500

    make voice pure

--- src/say.py
+++ src/say.py
@@ -1,6 +1,15 @@
 from subprocess import Popen, PIPE, STDOUT
 
 
+class VoiceEffect(object):
+    def __init__(self, phrase):
+        self.phrase = phrase
+
+    def run(self):
+        process = Popen(["say {}".format(self.phrase)], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, )
+        output, _ = process.communicate()
+        return output
+
+
 def say(phrase):
-    process = Popen(['say {}'.format(phrase)], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, )
-    output, _ = process.communicate()
+    return VoiceEffect(phrase)
---
+++ src/test_commands.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestCommands(TestCase):
+
+    def test_next(self):
+        self.assertEqual(
+            next(),
+            [
+
+            ]
+        )
