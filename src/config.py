105dd24f655c9b7e15dbd966db878630b961872b
Tavish Pegram <tapegram@gmail.com>
2020-06-18 19:32:25 -0500

    some config behavior

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import os
-from subprocess import Popen, PIPE
+import json
 
 
 class GetEnvironmentVariableEffect(object):
@@ -15,3 +15,46 @@ MOBN_WORKSTREAM_NAME = "MOBN_WORKSTREAM_NAME"
 
 def get_workstream_name():
     return GetEnvironmentVariableEffect(MOBN_WORKSTREAM_NAME)
+
+
+DEFAULT_CONFIG = dict(
+    team_members=[],
+    turn=0,
+)
+
+
+class GetConfigEffect(object):
+    def __init__(self, path):
+        self.path = path
+
+    def run(self):
+        if not os.path.exists(self.path):
+            return DEFAULT_CONFIG
+
+        with open(self.path) as config_file:
+            return json.load(config_file)
+
+
+def get_config(path_to_config):
+    return GetConfigEffect(path_to_config)
+
+
+class SetConfigEffect(object):
+    def __init__(self, path, config):
+        self.path = path
+        self.config = config
+
+    def run(self):
+        with open(self.path, "w+") as config_file:
+            json.dump(
+                self.config,
+                config_file,
+                indent=4
+            )
+
+
+def set_config(path_to_config, config):
+    return SetConfigEffect(
+        path=path_to_config,
+        config=config,
+    )
