f909d9401839425122688bb89b120d6692c969bb
Patrick Cupka <patrick.cupka@onepeloton.com>
2020-08-04 15:25:30 -0400

    update voice and phrases to be more polite (#11)
    * update voice and phrases to be more polite
    
    * remove stray print

--- src/domain/say.py
+++ src/domain/say.py
@@ -1,12 +1,14 @@
 from subprocess import Popen, PIPE, STDOUT
 
+import os
 
 class VoiceEffect(object):
     def __init__(self, phrase):
         self.phrase = phrase
 
     def run(self):
-        process = Popen(["say {}".format(self.phrase)], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, )
+        voice = os.environ['MOBN_VOICE'] or "Daniel"
+        process = Popen(["say -v {} {}".format(voice, self.phrase)], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, )
         output, _ = process.communicate()
         return output
 
